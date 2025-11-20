3cfe44561181ed1ce4fc16ea9553de0c4d041b42
danielecelsa <77167372+danielecelsa@users.noreply.github.com>
2025-11-20 11:50:44 +0100

    rollback uvicorn

--- server.py
+++ server.py
@@ -73,7 +73,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.mount("/tav", mcp.streamable_http_app())
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=10000)
